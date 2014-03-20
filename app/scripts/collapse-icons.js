f33f949054cb4faccdf0f42aa26b8361e719e59d
Gary A. Stafford <garystafford@rochester.rr.com>
2014-03-19 21:19:42 -0400

    fixing glyphicons

--- app/scripts/collapse-icons.js
+++ app/scripts/collapse-icons.js
@@ -1,36 +1,45 @@
 'use strict';
 
-$(document).on("click", 'a#collapseOneTitle.pull-right', function() {
-  $("#collapseOneGlyph").removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-up");
-});
+/*
+ $(document).on("click", 'a#collapseOneTitle.pull-right', function () {
+ $("#collapseOneGlyph").removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-up");
+ });
 
-$(document).on("click", 'a#collapseOneTitle.pull-right.collapsed', function() {
-  $("#collapseOneGlyph").removeClass("glyphicon-chevron-up").addClass("glyphicon-chevron-down");
-});
-
-
-$(document).on("click", 'a#collapseTwoTitle.pull-right', function() {
-  $("#collapseTwoGlyph").removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-up");
-});
-
-$(document).on("click", 'a#collapseTwoTitle.pull-right.collapsed', function() {
-  $("#collapseTwoGlyph").removeClass("glyphicon-chevron-up").addClass("glyphicon-chevron-down");
-});
+ $(document).on("click", 'a#collapseOneTitle.pull-right.collapsed', function () {
+ $("#collapseOneGlyph").removeClass("glyphicon-chevron-up").addClass("glyphicon-chevron-down");
+ });
 
 
-$(document).on("click", 'a#collapseThreeTitle.pull-right', function() {
-  $("#collapseThreeGlyph").removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-up");
-});
+ $(document).on("click", 'a#collapseTwoTitle.pull-right', function () {
+ $("#collapseTwoGlyph").removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-up");
+ });
 
-$(document).on("click", 'a#collapseThreeTitle.pull-right.collapsed', function() {
-  $("#collapseThreeGlyph").removeClass("glyphicon-chevron-up").addClass("glyphicon-chevron-down");
-});
+ $(document).on("click", 'a#collapseTwoTitle.pull-right.collapsed', function () {
+ $("#collapseTwoGlyph").removeClass("glyphicon-chevron-up").addClass("glyphicon-chevron-down");
+ });
 
 
-$(document).on("click", 'a#collapseFourTitle.pull-right', function() {
-  $("#collapseFourGlyph").removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-up");
-});
+ $(document).on("click", 'a#collapseThreeTitle.pull-right', function () {
+ $("#collapseThreeGlyph").removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-up");
+ });
 
-$(document).on("click", 'a#collapseFourTitle.pull-right.collapsed', function() {
-  $("#collapseFourGlyph").removeClass("glyphicon-chevron-up").addClass("glyphicon-chevron-down");
-});
+ $(document).on("click", 'a#collapseThreeTitle.pull-right.collapsed', function () {
+ $("#collapseThreeGlyph").removeClass("glyphicon-chevron-up").addClass("glyphicon-chevron-down");
+ });
+
+
+ $(document).on("click", 'a#collapseFourTitle.pull-right', function () {
+ $("#collapseFourGlyph").removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-up");
+ });
+
+ $(document).on("click", 'a#collapseFourTitle.pull-right.collapsed', function () {
+ $("#collapseFourGlyph").removeClass("glyphicon-chevron-up").addClass("glyphicon-chevron-down");
+ });
+ */
+var flipChevron = function (obj) {
+  if (obj.hasClass('collapsed')) {
+    obj[0].firstElementChild.className = "glyphicon glyphicon-chevron-down";
+  } else {
+    obj[0].firstElementChild.className = "glyphicon glyphicon-chevron-up";
+  }
+};
