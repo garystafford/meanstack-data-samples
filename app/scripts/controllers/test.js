49329517bd349b6b162f0a68bd14c4a5fefd2edd
Gary A. Stafford <garystafford@rochester.rr.com>
2014-03-21 11:26:41 -0400

    updating comments and text on pages.

--- app/scripts/controllers/test.js
+++ app/scripts/controllers/test.js
@@ -1,9 +1,9 @@
 'use strict';
 
-// Function within AngularJS controller contains array of strings
+// Function within the AngularJS Controller returns a static array of strings.
 angular.module('generatorMeanstackApp')
-  .controller('TestCtrl', function($scope, $http, $location, $anchorScroll, TestService, testFactory, googleSearchFactory) {
-    $scope.yeomanStuff = function() {
+  .controller('TestCtrl', function ($scope, $http, $location, $anchorScroll, TestService, testFactory, googleSearchFactory) {
+    $scope.yeomanStuff = function () {
       return [
         'yo',
         'Grunt',
@@ -11,50 +11,54 @@ angular.module('generatorMeanstackApp')
       ];
     };
 
-    // AngularJS service returns an array of simple object literals to controller
+    // AngularJS Service returns a static array of object literals to the controller.
     $scope.meanStuff = {};
     var promise = TestService.getMeanStuff();
-    promise.then(function(data) {
+    promise.then(function (data) {
       $scope.meanStuff = data;
     });
 
-    // AngularJS factory returns contents of JSON file to controller
+    // AngularJS Factory returns the contents of JSON file to the controller.
     $scope.otherStuff = {};
     promise = testFactory.getOtherStuff();
-    promise.then(function(data) {
+    promise.then(function (data) {
       $scope.otherStuff = data;
     });
 
-    // MongoDB database returns collection of documents to controller
+    // MongoDB Database returns a collection of documents to the controller.
     $http.get('/api/components')
-      .success(function(data) {
+      .success(function (data) {
         $scope.mongoStuff = data;
       })
-      .error(function(data) {
+      .error(function (data) {
         console.log('Error: ' + data);
       });
 
-    $scope.scrollTo = function(id) {
+    // AngularJS Factory returns results from
+    // Google's RESTful Web Search API to the controller.
+    $scope.googleStuff = {};
+    promise = googleSearchFactory.getSearchResults();
+    promise.then(function (data) {
+      $scope.googleStuff = data;
+    });
+
+    // Return to top (or other anchor)
+    $scope.scrollTo = function (id) {
       $location.hash(id);
       $anchorScroll();
     };
 
-    // Used to change between views using button
-    $scope.changeView = function(view) {
+    // Switch between views using button
+    $scope.changeView = function (view) {
       $location.path(view); // path not hash
     };
 
-    $scope.flipChevron = function($event) {
+    // Flip chevron on click
+    $scope.flipChevron = function ($event) {
       if ($event.target.parentElement.className == 'pull-right collapsed') {
         $event.target.className = 'glyphicon glyphicon-chevron-up';
       } else {
         $event.target.className = 'glyphicon glyphicon-chevron-down';
       }
     };
-
-    $scope.googleStuff = {};
-    promise = googleSearchFactory.getSearchResults();
-    promise.then(function(data) {
-      $scope.googleStuff = data;
-    });
   });
