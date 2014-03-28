d6c89e40f06fe40a4ad68dd6c569309a6675b6c5
Gary Stafford <gstafford@paychex.com>
2014-03-28 16:37:01 -0400

    remove promise from meanService example. Not neccessary.

--- app/scripts/controllers/DataController.js
+++ app/scripts/controllers/DataController.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('generatorMeanstackApp')
-  .controller('DataController', function ($scope, $http, $location, $anchorScroll, meanService, jsonFactory, mongoFactory, googleFactory) {
+  .controller('DataController', function($scope, $http, $location, $anchorScroll, meanService, jsonFactory, mongoFactory, googleFactory) {
 
     // Function within the AngularJS Controller returns a static array of strings.
-    $scope.yeomanStuff = function () {
+    $scope.yeomanStuff = function() {
       return [
         'yo',
         'Grunt',
@@ -14,59 +14,57 @@ angular.module('generatorMeanstackApp')
 
     // AngularJS Service returns a static array of object literals to the controller.
     $scope.meanStuff = {};
-    meanService.getMeanStuff()
-      .then(function (data) { // success
-        $scope.meanStuff = data;
-      },
-      function () {// failure
-        console.log('meanService.getMeanStuff() failed');
-      });
+    try {
+      $scope.meanStuff = meanService.getMeanStuff();
+    } catch (err) {
+      console.log('meanService.getMeanStuff() failed');
+    }
 
     // AngularJS Factory returns the contents of JSON file to the controller.
     $scope.otherStuff = {};
     jsonFactory.getOtherStuff()
-      .then(function (data) {
-        $scope.otherStuff = data;
-      },
-      function () {// failure
-        console.log('jsonFactory.getOtherStuff() failed');
-      });
+      .then(function(data) {
+          $scope.otherStuff = data;
+        },
+        function() { // failure
+          console.log('jsonFactory.getOtherStuff() failed');
+        });
 
     // AngularJS Factory returns a collection of documents
     // from MongoDB Database to the controller.
     $scope.otherStuff = {};
     mongoFactory.getMongoStuff()
-      .then(function (data) {
-        $scope.mongoStuff = data;
-      },
-      function () {// failure
-        console.log('mongoFactory.getMongoStuff() failed');
-      });
+      .then(function(data) {
+          $scope.mongoStuff = data;
+        },
+        function() { // failure
+          console.log('mongoFactory.getMongoStuff() failed');
+        });
 
     // AngularJS Factory returns results from
     // Google's RESTful Web Search API to the controller.
     $scope.googleStuff = {};
     googleFactory.getSearchResults()
-      .then(function (data) {
-        $scope.googleStuff = data;
-      },
-      function () {// failure
-        console.log('googleFactory.getSearchResults() failed');
-      });
+      .then(function(data) {
+          $scope.googleStuff = data;
+        },
+        function() { // failure
+          console.log('googleFactory.getSearchResults() failed');
+        });
 
     // Return to top (or other anchor)
-    $scope.scrollTo = function (id) {
+    $scope.scrollTo = function(id) {
       $location.hash(id);
       $anchorScroll();
     };
 
     // Switch between views using button
-    $scope.changeView = function (view) {
+    $scope.changeView = function(view) {
       $location.path(view); // path not hash
     };
 
     // Flip chevron on click
-    $scope.flipChevron = function ($event) {
+    $scope.flipChevron = function($event) {
       if ($event.target.parentElement.className === 'pull-right collapsed') {
         $event.target.className = 'glyphicon glyphicon-chevron-up';
       } else {
--- app/scripts/services/meanService.js
+++ app/scripts/services/meanService.js
@@ -1,33 +1,24 @@
 'use strict';
 
 angular.module('generatorMeanstackApp')
-  .service('meanService', function ($q) {
-    this.getMeanStuff = function () {
-      var deferred = $q.defer();
-
+  .service('meanService', function() {
+    this.getMeanStuff = function() {
       var components = {
-        components: [
-          {
-            component: 'MongoDB',
-            url: 'http://www.mongodb.org'
-          },
-          {
-            component: 'Express',
-            url: 'http://expressjs.com'
-          },
-          {
-            component: 'AngularJS',
-            url: 'http://angularjs.org'
-          },
-          {
-            component: 'Node.js',
-            url: 'http://nodejs.org'
-          }
-        ]
+        components: [{
+          component: 'MongoDB',
+          url: 'http://www.mongodb.org'
+        }, {
+          component: 'Express',
+          url: 'http://expressjs.com'
+        }, {
+          component: 'AngularJS',
+          url: 'http://angularjs.org'
+        }, {
+          component: 'Node.js',
+          url: 'http://nodejs.org'
+        }]
       };
 
-      deferred.resolve(components);
-
-      return deferred.promise;
+      return components;
     };
   });
