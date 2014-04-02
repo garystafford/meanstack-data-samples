d3ce1ee74099c0e13729448123b2ae35727c4be1
Gary A. Stafford <garystafford@rochester.rr.com>
2014-04-01 23:59:03 -0400

    creating pre-load script. Unfinished.

--- mongo-setup.js
+++ mongo-setup.js
@@ -4,23 +4,23 @@ var Db = require('mongodb').Db,
   Server = require('mongodb').Server,
   Code = require('mongodb').Code,
   assert = require('assert'),
-  db = new Db('meanstack-test', new Server('localhost', 27017));
+  db = new Db('meanstack-test', new Server('localhost', 27017), {w: 1});
 
 // Establish connection to db
 db.open(function (err, db) {
+// Crete the collection
+  db.createCollection('components2', {strict: true}, function (err, collection) {
+    if (!err===err) console.error(err);
+    exit;
 
-  if (isNull(db.getCollection('components2').exists())) {
+    // Insert documents to perform distinct against
+    collection.insert({ "component": "mongod", "description": "core database process" });
+    collection.insert({ "component": "mongos", "description": "controller and query router for sharded clusters" });
+    collection.insert({ "component": "mongo", "description": "interactive MongoDB Shell" });
+    collection.insert({ "component": "mongodump", "description": "utility for creating binary export of database contents" });
+    collection.insert({ "component": "mongorestore", "description": "writes data from a binary database dump to a MongoDB instance" });
+    collection.insert({ "component": "mongooplog", "description": "polls operations from the replication oplog" });
 
-    // Crete the collection
-    db.createCollection('components2', function (err, collection) {
-
-      // Insert documents to perform distinct against
-      collection.insert({ "component": "mongod", "description": "core database process" });
-      collection.insert({ "component": "mongos", "description": "controller and query router for sharded clusters" });
-      collection.insert({ "component": "mongo", "description": "interactive MongoDB Shell" });
-      collection.insert({ "component": "mongodump", "description": "utility for creating binary export of database contents" });
-      collection.insert({ "component": "mongorestore", "description": "writes data from a binary database dump to a MongoDB instance" });
-      collection.insert({ "component": "mongooplog", "description": "polls operations from the replication oplog" });
-    });
-  }
+    db.close();
+  });
 });
