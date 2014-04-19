250da402aafa4e0ac4c4bf72a100d551b33fedbc
Gary A. Stafford <garystafford@rochester.rr.com>
2014-04-18 23:28:55 -0400

    Adding Travis CI configuration

---
+++ config/environments/travis.js
@@ -0,0 +1,29 @@
+var express = require('express'),
+    path = require('path');
+
+module.exports = function(app) {
+    app.configure('travis', function() {
+        app.use(function staticsPlaceholder(req, res, next) {
+            return next();
+        });
+        app.set('db', 'mongodb://localhost/meanstack-travis');
+        app.set('name', 'A generator-meanstack sample application - Travis');
+        app.set('port', process.env.PORT || 9000);
+        app.set('views', path.join(app.directory, '/app'));
+        app.engine('html', require('ejs').renderFile);
+        app.set('view engine', 'html');
+        app.use(express.favicon());
+        app.use(express.logger('dev'));
+        app.use(express.bodyParser());
+        app.use(express.methodOverride());
+        app.use(express.cookieParser('your secret here'));
+        app.use(express.session());
+
+        app.use(function middlewarePlaceholder(req, res, next) {
+            return next();
+        });
+
+        app.use(app.router);
+        app.use(express.errorHandler());
+    });
+};
