b7532ba0a7c9ed78bbbc11a5449972d2933feba3
Gary A. Stafford <garystafford@rochester.rr.com>
2014-04-19 09:26:14 -0400

    fixing dependencies

--- karma.conf.js
+++ karma.conf.js
@@ -48,10 +48,10 @@ autoWatch = false;
 // - Safari (only Mac)
 // - PhantomJS
 // - IE (only Windows)
-browsers = ['PhantomJS'];
+browsers = ['Chrome'];
 
 // If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
+captureTimeout = 10000;
 
 // Continuous Integration mode
 // if true, it capture browsers, run tests and exit
